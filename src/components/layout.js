b2dfbefa72755999006eea11023128ebeafd88f2
Favio Valero <favio102@protonmail.com>
2023-09-21 05:38:08 -0700

    deploy to netlify settings

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const Layout = ({ children }) => {
